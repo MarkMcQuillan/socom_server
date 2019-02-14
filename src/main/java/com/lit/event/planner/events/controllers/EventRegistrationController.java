ecb5dc21d373684e34062b9f1b2c7489e565ba8b
n0361402 <Mark.McQuillan"liberty-it.co.uk>
2019-02-14 16:48:12 +0000

    Added Controllers for registration and guests

--- src/main/java/com/lit/event/planner/events/controllers/EventRegistrationController.java
+++ src/main/java/com/lit/event/planner/events/controllers/EventRegistrationController.java
@@ -1,4 +1,76 @@
 package com.lit.event.planner.events.controllers;
 
+import com.lit.event.planner.events.EventRegistrationRepository;
+import com.lit.event.planner.events.exception.EventRegistrationNotFoundException;
+import com.lit.event.planner.events.model.EventRegistration;
+import com.lit.event.planner.events.services.EventRegistrationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import javax.validation.Valid;
+
+import java.util.List;
+
+// TODO: Extend model and database schema with EventRegistrations
+// TODO: Add EventRegistrationRepository
+// TODO: Use repository here to find eventRegistrations
+
+
+
+@RestController
+@RequestMapping("eventRegistrations")
 public class EventRegistrationController {
+
+    @Autowired
+    private EventRegistrationRepository eventRegistrationRepository ;
+
+    @Autowired
+    private EventRegistrationService eventRegistrationService;
+
+    //CONTROLLER FOR GET ALL
+    @GetMapping()
+    public List<EventRegistration> getAllEventRegistrations(){
+        return eventRegistrationService.getAllRegistrations();
+    }
+
+    //CONTROLLER FOR GET ALL BY ID
+    @GetMapping(value = "/{id}", produces = "application/json")
+    public EventRegistration getEventRegistration(@PathVariable ("id") int id){
+        return  eventRegistrationService.getRegistration(id)
+                .orElseThrow(() -> new EventRegistrationNotFoundException(id));
+    }
+
+    //CONTROLLER FOR ADDING
+    @RequestMapping(method= RequestMethod.POST, value = "/eventRegistration")
+    public void addEventRegistration(@RequestBody EventRegistration eventRegistration){
+        eventRegistrationService.addRegistration((eventRegistration));
+    }
+
+    //CONTROLLER FOR DELETION
+    @DeleteMapping("/{id}")
+    public void deleteEventRegistration(@PathVariable int id){
+        eventRegistrationService.deleteRegistration(id);
+    }
+
+    //CONTROLLER FOR UPDATING
+    @PutMapping("/{id}")
+    public ResponseEntity<EventRegistration> updateEventRegistration(@PathVariable(value = "id") int id,
+                                             @Valid @RequestBody EventRegistration eventRegistrationDetails) throws EventRegistrationNotFoundException {
+        EventRegistration eventRegistration = eventRegistrationRepository.findById(id)
+                .orElseThrow(() -> new EventRegistrationNotFoundException(id));
+
+        eventRegistration.setEmployeeID(eventRegistrationDetails.getEmployeeID());
+        eventRegistration.setEventID(eventRegistration.getEventID());
+        eventRegistration.setDate(eventRegistration.getDate());
+        eventRegistration.setGuestFeePaid(eventRegistration.isGuestFeePaid());
+        eventRegistration.setGuestID(eventRegistration.getGuestID());
+
+        final EventRegistration updatedEventRegistration = eventRegistrationRepository.save(eventRegistration);
+        return ResponseEntity.ok(updatedEventRegistration);
+    }
+
+
+
 }
--- src/main/java/com/lit/event/planner/events/controllers/GuestController.java
+++ src/main/java/com/lit/event/planner/events/controllers/GuestController.java
@@ -1,4 +1,82 @@
 package com.lit.event.planner.events.controllers;
 
+import com.lit.event.planner.events.EventRepository;
+import com.lit.event.planner.events.GuestRepository;
+import com.lit.event.planner.events.exception.EventNotFoundException;
+import com.lit.event.planner.events.exception.GuestNotFoundException;
+import com.lit.event.planner.events.model.Event;
+import com.lit.event.planner.events.model.Guest;
+import com.lit.event.planner.events.services.EventService;
+import com.lit.event.planner.events.services.GuestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.Valid;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+// TODO: Extend model and database schema with Events
+// TODO: Add EventRepository
+// TODO: Use repository here to find events
+
+
+
+@RestController
+@RequestMapping("events")
 public class GuestController {
+
+    @Autowired
+    private GuestRepository guestRepository ;
+
+    @Autowired
+    private GuestService guestService;
+
+    //CONTROLLER FOR GET ALL
+    @GetMapping()
+    public List<Guest> getAllGuests(){
+        return guestService.getAllGuests();
+    }
+
+    //CONTROLLER FOR GET ALL BY ID
+    @GetMapping(value = "/{id}", produces = "application/json")
+    public Guest getGuest(@PathVariable ("id") int id){
+        return  guestService.getGuest(id)
+                .orElseThrow(() -> new GuestNotFoundException(id));
+    }
+
+    //CONTROLLER FOR ADDING
+    @RequestMapping(method= RequestMethod.POST, value = "/guest")
+    public void addGuest(@RequestBody Guest guest){
+        guestService.addGuest((guest));
+    }
+
+    //CONTROLLER FOR DELETION
+    @DeleteMapping("/{id}")
+    public void deleteGuest(@PathVariable int id){
+        guestService.deleteGuest(id);
+    }
+
+    //CONTROLLER FOR UPDATING
+    @PutMapping("/{id}")
+    public ResponseEntity<Guest> updateGuest(@PathVariable(value = "id") int id,
+                                             @Valid @RequestBody Guest guestDetails) throws GuestNotFoundException {
+        Guest guest = guestRepository.findById(id)
+                .orElseThrow(() -> new GuestNotFoundException(id));
+
+        guest.setFirstName(guestDetails.getFirstName());
+        guest.setLastName(guestDetails.getLastName());
+        guest.setCreatedOn(guestDetails.getCreatedOn());
+        guest.setGuestID(guestDetails.getGuestID());
+
+        final Guest updatedGuest = guestRepository.save(guest);
+        return ResponseEntity.ok(updatedGuest);
+    }
+
+
+
 }
